1e498e05fab5c1426cc8a6e429a0463458de73e9
Rico Neitzel <rico@buro71a.de>
2020-09-11 20:53:21 +0200

    recognizes ACL settings in controller

--- src/app/code/community/Mageone/PatchList/controllers/Mageone/PatchlistController.php
+++ src/app/code/community/Mageone/PatchList/controllers/Mageone/PatchlistController.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 class Mageone_PatchList_Mageone_PatchlistController extends Mage_Adminhtml_Controller_Action
 {
+
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('admin');
+    }
+
     public function indexAction()
     {
         $this->loadLayout()->renderLayout();
